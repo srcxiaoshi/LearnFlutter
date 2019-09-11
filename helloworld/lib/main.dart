d5416ba1bb678cba37d7c59cb7610e7aa3be3cc2
srcxiaoshi <869259080@qq.com>
2019-09-11 14:34:45 +0800

    helloworld

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
-
+///入口
+void main(List<String> args) {
+  return runApp(MyApp());
+}
+///无状态类
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -9,22 +11,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Text('Hello World'),
+        ),
+      )
     );
   }
 }
-
+///有状态类
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
